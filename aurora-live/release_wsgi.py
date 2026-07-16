43a0bc30263f2b53cb7cd66efd81d1456bb47daa
hr185882-creator <hr185882@gmail.com>
2026-07-16 02:33:09 -0400

    Activate Phase 10 geographic application

--- aurora-live/release_wsgi.py
+++ aurora-live/release_wsgi.py
@@ -5,4 +5,4 @@ from phase8_runtime import OperationalAggregator
 
 app.AGGREGATOR = OperationalAggregator()
 
-from phase8_wsgi import application  # noqa: E402,F401
+from phase10_wsgi import application  # noqa: E402,F401
