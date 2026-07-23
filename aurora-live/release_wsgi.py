50424b9d12aebdd1c0fafe9519bc20bad00ebfeb
hr185882-creator <hr185882@gmail.com>
2026-07-23 00:12:32 -0400

    phase20: update aurora-live/release_wsgi.py

--- aurora-live/release_wsgi.py
+++ aurora-live/release_wsgi.py
@@ -5,4 +5,4 @@ from phase8_runtime import OperationalAggregator
 
 app.AGGREGATOR = OperationalAggregator()
 
-from phase19_complete import application  # noqa: E402,F401
+from phase20_complete import application  # noqa: E402,F401
