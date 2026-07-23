cfbf872c58c240755d990c89a1012af80ef08f11
hr185882-creator <hr185882@gmail.com>
2026-07-22 20:56:14 -0400

    Route release WSGI through Phase 18

--- aurora-live/release_wsgi.py
+++ aurora-live/release_wsgi.py
@@ -5,4 +5,4 @@ from phase8_runtime import OperationalAggregator
 
 app.AGGREGATOR = OperationalAggregator()
 
-from phase17_complete import application  # noqa: E402,F401
+from phase18_complete import application  # noqa: E402,F401
